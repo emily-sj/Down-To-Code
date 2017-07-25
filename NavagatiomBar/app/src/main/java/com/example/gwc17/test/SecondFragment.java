b3184c0b05452bfc696d25a918a12101ceee4987
charlenecalderon <charlenecalderon14@gmail.com>
2017-07-25 10:52:06 -0700

    navbar
    test

---
+++ NavagatiomBar/app/src/main/java/com/example/gwc17/test/SecondFragment.java
@@ -0,0 +1,27 @@
+package com.example.gwc17.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by GWC17 on 7/25/2017.
+ */
+
+public class SecondFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return myView;
+    }
+}
+
+
+
---
+++ NavagatiomBar/app/src/main/java/com/example/gwc17/test/ThirdFragment.java
@@ -0,0 +1,28 @@
+package com.example.gwc17.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by GWC17 on 7/25/2017.
+ */
+
+public class ThirdFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.third_layout, container, false);
+        return myView;
+
+    }
+}
+
+
+
